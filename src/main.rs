5263a802cd3bfd1f9393228444aa6262ca5bbc28
Michael Darr <michael.e.darr@gmail.com>
2023-09-05 11:15:03 -0700

    Bring clock logic into package
    Signed-off-by: Michael Darr <michael.e.darr@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 #![no_std]
 #![no_main]
 
+const BLINK_DELAY: u16 = 500;
+
+mod clock;
+
+use embedded_hal::prelude::_embedded_hal_blocking_delay_DelayMs;
 use panic_halt as _;
 
 #[arduino_hal::entry]
@@ -9,8 +14,9 @@ fn main() -> ! {
     let dp = arduino_hal::Peripherals::take().unwrap();
     let pins = arduino_hal::pins!(dp);
     let mut led = pins.led_rx.into_output();
+
     loop {
         led.toggle();
-        arduino_hal::delay_ms(300);
+        clock::Delay::new().delay_ms(BLINK_DELAY);
     }
 }
