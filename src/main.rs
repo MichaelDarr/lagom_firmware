bab80d19ba598c0945eb1f72acd48e66c1094e09
Michael Darr <michael.e.darr@gmail.com>
2023-09-13 07:35:09 -0700

    Identify as a mad cat
    Signed-off-by: Michael Darr <michael.e.darr@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -55,7 +55,8 @@ fn main() -> ! {
         &*USB_BUS.insert(UsbBus::with_suspend_notifier(usb, pll))
     };
     let hid_class = HIDClass::new(usb_bus, KeyboardReport::desc(), 1);
-    let usb_device = UsbDeviceBuilder::new(usb_bus, UsbVidPid(0x1209, 0x0001))
+    // http://www.linux-usb.org/usb.ids
+    let usb_device = UsbDeviceBuilder::new(usb_bus, UsbVidPid(0x0738, 0x4588))
         .manufacturer("sthlm kb.")
         .product("Lagom")
         .build();
